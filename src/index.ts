c6ce21251c32df80107257d6a2e02f477720d317
krishna jain <krishnajain5050@gmail.com>
2024-05-28 19:34:52 +0530

    ffmpeg and docker added

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,33 @@ import http from 'node:http'
 import express from 'express'
 import path from 'node:path'
 import {Server} from 'socket.io'
+import {spawn} from 'child_process'
 
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server)
+
+const options: string[] = [
+    '-i',
+    '-',
+    '-c:v', 'libx264',
+    '-preset', 'ultrafast',
+    '-tune', 'zerolatency',
+    '-r', `${25}`,
+    '-g', `${25 * 2}`,
+    '-keyint_min', `25`,
+    '-crf', '25',
+    '-pix_fmt', 'yuv420p',
+    '-sc_threshold', '0',
+    '-profile:v', 'main',
+    '-level', '3.1',
+    '-c:a', 'aac',
+    '-b:a', '128k',
+    '-ar', `128000 / 4`,
+    '-f', 'flv',
+    `rtmp://a.rtmp.youtube.com/live2/dcfx-m7v2-j248-3185-9207`,
+]
+const ffmpegProcess = spawn('ffmpeg', options)
 
 app.use(express.static(path.resolve('./public')))
 app.use(express.json())
@@ -13,7 +36,9 @@ app.use(express.json())
 io.on('connection', socket => {
     console.log(socket.id)
     socket.on('binary-stream', stream => {
-        console.log("Stream incoming...")
+        ffmpegProcess.stdin.write(stream, err => {
+            console.log(err)
+        })
     })
 })
 
